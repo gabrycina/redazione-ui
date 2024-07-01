291a515f824880bfa2e59b544991e5fe3f937106
gabrycina <c.gabriele.info@gmail.com>
2024-07-01 11:55:32 +0100

    Update key

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { PostHogProvider } from 'posthog-js/react'
+import posthog from "posthog-js"
 
-const options = {
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-}
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_HOSTING_KEY}
-      options={options}
-    >
+    <PostHogProvider client={posthog} >
       <App />
     </PostHogProvider>
   </React.StrictMode>,
